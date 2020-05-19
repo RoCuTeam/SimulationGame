acf27952ca7120c13dba882a8c1b99a9f9027639
Cuauhtli Fernando <cuauh_fer_bh@hotmail.com>
2020-05-19 14:08:21 -0500

    Mapa creado, disparos y enemigos

--- js/index.js
+++ js/index.js
@@ -21,19 +21,42 @@ var config = {
 var player;
 var stars;
 var enemies;
+var ballons = ['Blue', 'Cyan', 'Gold', 'Green', 'Pink', 'Red', 'White', 'Yellow'];
+var attacks;
 var platforms;
 var cursors;
 
 // Contadores y banderas
 var score = 0;
-var health = 3;
+var health = 5;
+var enemiesNum = 0;
 var inventory = {
   "polvore": 0,
   "dinamite": 0,
-  "shots": 0
+  "shots": 5
 }
+var coldowns = {
+  'tintSecs': 50,
+  'attackReload': 30,
+  'manaReload': 2000,
+  'turnPause': 50,
+  'nextEnemy': 500,
+  'buildTime': 2500
+}
+// Contadores para recargas o eventos
 var tintSecs = 0;
+var attackSecs = 0;
+var turnPause = 0;
+var building = coldowns['buildTime'];
+var nextEnemy = coldowns['nextEnemy'];
+var manaSecs = coldowns['manaReload'];
+
 var gameOver = false;
+var paused = true;
+var lastDirection = 'Front';
+var loadAttacks = false;
+
+const baseVelocity = 160;
 
 var game = new Phaser.Game(config);
 
@@ -42,14 +65,38 @@ function preload (){
     this.load.image('floor', 'assets/floor.jpg');
     // Paredes y obstaculos
     this.load.image('singleRock', 'assets/rock.png');
+    this.load.image('lineRocks2', 'assets/rockLinex2.png');
+    this.load.image('lineRocks3', 'assets/rockLinex3.png');
+    this.load.image('lineRocks4', 'assets/rockLinex4.png');
     this.load.image('lineRocks', 'assets/rockLine.png');
+    this.load.image('columnRocks2', 'assets/rockColumnx2.png');
+    this.load.image('columnRocks3', 'assets/rockColumnx3.png');
+    this.load.image('columnRocks4', 'assets/rockColumnx4.png');
+    this.load.image('columnRocks', 'assets/rockColumn.png');
+
+    //Fortaleza
+    this.load.image('column', 'assets/rockColumnx4.png');
+    this.load.image('line', 'assets/rockLinex4.png');
+    this.load.image('singleEnemy', 'assets/rock.png');
+
     // Objetos
     this.load.image('tnt', 'assets/dinamite.png');
     this.load.image('polvore', 'assets/polvore.png');
+    this.load.spritesheet('fireball', 'assets/fireballSprite.png', { frameWidth: 25, frameHeight: 25 });
+    this.load.spritesheet('explosion', 'assets/explosion.png', { frameWidth: 25, frameHeight: 25 });
     // Enemigos
-    this.load.image('enemy', 'assets/enemy1.png');
+    this.load.spritesheet('ballonBlue', 'assets/enemyBlue.png',  { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('ballonCyan', 'assets/enemyCyan.png',  { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('ballonGold', 'assets/enemyGold.png',  { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('ballonGreen', 'assets/enemyGreen.png',  { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('ballonPink', 'assets/enemyPink.png',  { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('ballonRed', 'assets/enemyRed.png',  { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('ballonWhite', 'assets/enemyWhite.png',  { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('ballonYellow', 'assets/enemyYellow.png',  { frameWidth: 32, frameHeight: 48 });
     // Jugador
     this.load.spritesheet('red', 'assets/red.png', { frameWidth: 32, frameHeight: 48 });
+    // canvas
+    this.load.image('pauseBtn', 'assets/pause.png');
 }
 
 function create (){
@@ -62,10 +109,52 @@ function create (){
 
     // Grupo de elementos estaticos, seran nuestras paredes y obstaculos
     platforms = this.physics.add.staticGroup();
+    fortress = this.physics.add.staticGroup();
 
     // Grupo de paredes y obstaculos en el mapa
-    platforms.create(250, 450, 'singleRock').setScale(1).refreshBody();
-    platforms.create(300, 150, 'lineRocks');
+    //Fortaleza
+    fortress.create(960, 1050, 'singleEnemy');
+    fortress.create(835, 976, 'column');
+    fortress.create(1085, 976, 'column');
+    fortress.create(960, 890, 'line');
+
+    //Cuadrante uno del mapa
+    platforms.create(150, 100, 'lineRocks2');       //horizontal, vertical
+    platforms.create(700, 500, 'lineRocks');
+    platforms.create(150, 600, 'lineRocks3');
+
+    platforms.create(225, 300, 'columnRocks4');
+    platforms.create(500, 125, 'columnRocks');
+    platforms.create(800, 250, 'columnRocks3');
+    platforms.create(1050, 100, 'columnRocks2');
+    platforms.create(1000, 300, 'columnRocks2');
+    platforms.create(1050, 500, 'columnRocks2');
+
+    //Cuadrante dos del mapa
+    platforms.create(870, 30, 'lineRocks3');
+    platforms.create(1400, 100, 'lineRocks');
+    platforms.create(1700, 80, 'columnRocks3');
+    platforms.create(1440, 400, 'columnRocks');
+    platforms.create(1815, 350, 'lineRocks4');
+    platforms.create(1230, 380, 'lineRocks3');
+    platforms.create(1230, 220, 'singleRock');
+    platforms.create(1650, 600, 'lineRocks');
+
+    //Cuadrante tres del mapa
+    platforms.create(150, 850, 'columnRocks3');
+    platforms.create(950, 700, 'columnRocks3');
+    platforms.create(400, 600, 'columnRocks');
+    platforms.create(450, 970, 'lineRocks4');
+    platforms.create(700, 700, 'lineRocks3');
+    platforms.create(670, 870, 'columnRocks2');
+
+    //Cuadrante cuatro del mapa
+    platforms.create(1200, 700, 'lineRocks4');
+    platforms.create(1300, 1000, 'columnRocks3');
+    platforms.create(1840, 800, 'lineRocks3');
+    platforms.create(1500, 850, 'columnRocks');
+    platforms.create(1670, 920, 'lineRocks2');
+    platforms.create(1800, 1030, 'columnRocks2');
 
     // Crear el personaje
     player = this.physics.add.sprite(0, 0, 'red');
@@ -108,65 +197,115 @@ function create (){
     });
     // Detenido
     this.anims.create({
-        key: 'turn',
+        key: 'turnFront',
         frames: [ { key: 'red', frame: 0 } ],
         frameRate: 20
     });
+    this.anims.create({
+        key: 'turnLeft',
+        frames: [ { key: 'red', frame: 4 } ],
+        frameRate: 20
+    });
+    this.anims.create({
+        key: 'turnRight',
+        frames: [ { key: 'red', frame: 8 } ],
+        frameRate: 20
+    });
+    this.anims.create({
+        key: 'turnBack',
+        frames: [ { key: 'red', frame: 12 } ],
+        frameRate: 20
+    });
 
-    // Controles
-    // Se asignan los controles para mover a nuestro jugador (W-A-S-D)
+    // Animaciones de los enemigos
+    for (i in ballons){
+      this.anims.create({
+        key: ballons[i]+'IDLE',
+        frames: this.anims.generateFrameNumbers('ballon'+ballons[i], { start: 0, end: 2 }),
+        frameRate: 10,
+        repeat: -1
+      });
+    }
+
+    // Animación de bola de fuego
+    this.anims.create({
+      key: 'fireSpin',
+      frames: this.anims.generateFrameNumbers('fireball', { start: 0, end: 3 }),
+      frameRate: 15,
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'explosion',
+      frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 15 }),
+      frameRate: 16
+    })
+
+    // Marcador de vida
+    for(var i = 1; i <= health; i++){
+      $('.health').append('<img id="health'+i+'" src="assets/heart.png" width="30px" height="30px">');
+    }
+
+    // Controles: Se asignan los controles para mover a nuestro jugador (W-A-S-D)
     cursors = this.input.keyboard.addKeys({
-        up:Phaser.Input.Keyboard.KeyCodes.W,
-        down:Phaser.Input.Keyboard.KeyCodes.S,
-        left:Phaser.Input.Keyboard.KeyCodes.A,
-        right:Phaser.Input.Keyboard.KeyCodes.D
+        up:     Phaser.Input.Keyboard.KeyCodes.W,
+        down:   Phaser.Input.Keyboard.KeyCodes.S,
+        left:   Phaser.Input.Keyboard.KeyCodes.A,
+        right:  Phaser.Input.Keyboard.KeyCodes.D,
+        attack: Phaser.Input.Keyboard.KeyCodes.SPACE,
+        pause:  Phaser.Input.Keyboard.KeyCodes.P,
+        start:  Phaser.Input.Keyboard.KeyCodes.ENTER
     });
 
     // Elementos que se pueden recolectar
     polvore = this.physics.add.group({
         key: 'polvore',
-        repeat: 2,
+        repeat: 4,
         setXY: { x: Phaser.Math.Between(0, 1920), y: Phaser.Math.Between(0, 1080) }
     });
 
     // Enemigos
     enemies = this.physics.add.group();
+    // Ataques del jugador
+    attacks = this.physics.add.group();
 
     // Colisiones en el juego
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(polvore, platforms);
     this.physics.add.collider(enemies, platforms);
+    this.physics.add.collider(platforms, attacks, deleteFireball, null, this);
 
     // Eventos
     // Si el jugador toca la polvora
     this.physics.add.overlap(player, polvore, collectPolvore, null, this);
+    // Si el jugador es tocado por un enemigo
+    this.physics.add.collider(player, enemies, hitEnemy, null, this);
+
     // Si la polvora se sobrepone a una plataforma o a otra polvora
     this.physics.add.overlap(platforms, polvore, movePolvore, null, this);
     this.physics.add.overlap(polvore, polvore, movePolvore, null, this);
-    // Si el jugador es tocado por un enemigo
-    this.physics.add.collider(player, enemies, hitEnemy, null, this);
+
+    // Si la bola de fuego toca un enemigo
+    this.physics.add.overlap(attacks, enemies, dmgEnemy, null, this);
 }
 
 function update (){
+  if(paused == false){
+    // Si se termino la vida
     if (gameOver){
         return;
     }
 
-    if(tintSecs != 0){
-      tintSecs -= 1;
-      if(tintSecs <= 0){
-        player.setTint();
-      }
-    }
-
+    // Movimiento del personaje
     if (cursors.up.isDown){
-      player.setVelocityY(-160);
+      lastDirection = 'Back';
+      player.setVelocityY(-baseVelocity);
+
       if (cursors.left.isDown){
-        player.setVelocityX(-160);
+        player.setVelocityX(-baseVelocity);
         player.anims.play('left', true);
       }
       else if (cursors.right.isDown){
-        player.setVelocityX(160);
+        player.setVelocityX(baseVelocity);
         player.anims.play('right', true);
       }
       else{
@@ -175,14 +314,15 @@ function update (){
       }
     }
     else if(cursors.down.isDown){
-      player.setVelocityY(160);
+      lastDirection = 'Front';
+      player.setVelocityY(baseVelocity);
 
       if (cursors.left.isDown){
-        player.setVelocityX(-160);
+        player.setVelocityX(-baseVelocity);
         player.anims.play('left', true);
       }
       else if (cursors.right.isDown){
-        player.setVelocityX(160);
+        player.setVelocityX(baseVelocity);
         player.anims.play('right', true);
       }
       else{
@@ -191,13 +331,15 @@ function update (){
       }
     }
     else if (cursors.left.isDown){
+      lastDirection = 'Left';
       player.setVelocityY(0);
-      player.setVelocityX(-160);
+      player.setVelocityX(-baseVelocity);
       player.anims.play('left', true);
     }
     else if (cursors.right.isDown){
+      lastDirection = 'Right';
       player.setVelocityY(0);
-      player.setVelocityX(160);
+      player.setVelocityX(baseVelocity);
       player.anims.play('right', true);
     }
 
@@ -205,9 +347,70 @@ function update (){
     if(cursors.left.isUp && cursors.right.isUp && cursors.up.isUp && cursors.down.isUp){
       player.setVelocityX(0);
       player.setVelocityY(0);
-      player.anims.play('turn');
+      player.anims.play('turn'+lastDirection);
     }
+    // Ataque del personaje
+    if(cursors.attack.isDown && attackSecs <= 0 && inventory['shots'] > 0){
+      var fireball = attacks.create(player.x, player.y, 'fireball');
+      fireball.setBounce(0);
+      fireball.outOfBoundsKill = true;
+      if(player.body.velocity.x == 0 && player.body.velocity.y == 0){
+        if(lastDirection == 'Right'){
+          fireball.setVelocity(baseVelocity*2, 0);
+        }
+        if(lastDirection == 'Left'){
+          fireball.setVelocity(-baseVelocity*2, 0);
+        }
+        if(lastDirection == 'Back'){
+          fireball.setVelocity(0, -baseVelocity*2);
+        }
+        if(lastDirection == 'Front'){
+          fireball.setVelocity(0, baseVelocity*2);
+        }
+      }
+      else{
+        fireball.setVelocity(player.body.velocity.x*2, player.body.velocity.y*2);
+      }
+      fireball.play('fireSpin');
+      fireball.allowGravity = false;
+      attackSecs = coldowns['attackReload'];
+      inventory['shots'] -= 1;
+    }
+    // Si quiere atacar pero no hay munición
+    if(cursors.attack.isDown && inventory['shots'] == 0){
+      $('#shots').css('background', 'rgba(127, 0, 0, 0.25)');
+      $('#shots').css('border', '1px solid rgba(127, 0, 0, 1)');
+    }
+
+    if(cursors.pause.isDown && turnPause == 0){
+      $('.pauseGame').fadeIn(100);
+      turnPause = coldowns['turnPause'];
+      player.setVelocityX(0);
+      player.setVelocityY(0);
+      player.anims.play('turnFront');
+      paused = true;
+      this.physics.pause();
+    }
+    // Actualizar contadores y UI
+    updateCounters();
+  }
+  else{
+    if((cursors.start.isDown && turnPause == 0)){
+      // (cursors.pause.isDown && turnPause == 0) ||
+      $('.pauseGame').fadeOut(100);
+      $('.startGame').fadeOut(100);
+      turnPause = coldowns['turnPause'];
+      player.setVelocity(0, 0);
+      paused = false;
+      this.physics.resume();
+    }
+  }
+  if(turnPause > 0){
+    turnPause -= 1;
+  }
 }
+
+// Eventos
 
 function collectPolvore (player, stack){
     stack.disableBody(true, true);
@@ -223,12 +426,6 @@ function collectPolvore (player, stack){
         polvore.children.iterate(function (child) {
             child.enableBody(true, Phaser.Math.Between(0, 1920), Phaser.Math.Between(0, 1080), true, true);
         });
-
-        var enemy = enemies.create(Phaser.Math.Between(0, 1920), Phaser.Math.Between(0, 1080), 'enemy').setScale(0.5);
-        enemy.setBounce(1);
-        enemy.setCollideWorldBounds(true);
-        enemy.setVelocity(Phaser.Math.Between(-300, 300), Phaser.Math.Between(-300, 300));
-        enemy.allowGravity = false;
     }
 }
 
@@ -241,6 +438,7 @@ function hitEnemy (player, enemy){
     enemy.disableBody(true, true);
     $('#health' + health).remove();
     health -= 1;
+    enemiesNum -= 1;
 
     if(health <= 0){
       this.physics.pause();
@@ -249,7 +447,116 @@ function hitEnemy (player, enemy){
       gameOver = true;
     }
     else{
-      tintSecs = 50;
+      tintSecs = coldowns['tintSecs'];
       player.setTint(0xff0000);
     }
+}
+
+function dmgEnemy(fireball, enemy){
+  fireball.setVelocity(0, 0);
+  fireball.play('explosion');
+  fireball.on('animationcomplete', (fire)=>{
+    fireball.disableBody(true, true);
+  });
+  enemy.disableBody(true, true);
+
+  enemiesNum -= 1;
+  score += 50;
+  $('.score p').html("Score: " + score);
+
+  inventory['shots'] += 1;
+}
+
+function deleteFireball(platform, fireball){
+  fireball.play('explosion');
+  fireball.on('animationcomplete', (fire)=>{
+    fireball.disableBody(true, true);
+  });
+}
+
+// Funciones para UI y contadores
+function updateCounters(){
+
+  // Si el jugador habia recibido daño
+  if(tintSecs != 0){
+    tintSecs -= 1;
+    if(tintSecs <= 0){
+      player.setTint();
+    }
+  }
+
+  // Si el jugador habia atacado
+  if(attackSecs != 0){
+    attackSecs -= 1;
+  }
+
+  // Si el jugador carga magia
+  if(manaSecs <= 0){
+    inventory['shots'] += 1;
+    manaSecs = coldowns['manaReload'];
+  }
+
+  // Reducir contador de magia
+  manaSecs -= 1;
+  nextEnemy -= 1;
+
+  if(nextEnemy <= 0){
+    addBallon();
+    nextEnemy = coldowns['nextEnemy'];
+  }
+  // Recargar la UI
+  refreshUI();
+}
+
+function addBallon(){
+  color = ballons[Phaser.Math.Between(0, 7)];
+  var enemy = enemies.create(Phaser.Math.Between(0, 1920), Phaser.Math.Between(0, 1080), 'ballon'+color);
+  enemy.setBounce(1);
+  enemy.setCollideWorldBounds(true);
+  enemy.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
+  enemy.play(color+'IDLE');
+  enemy.allowGravity = false;
+  enemiesNum += 1;
+}
+
+function refreshUI(){
+  manaPercent = 100 - parseInt((100 / coldowns['manaReload']) * manaSecs);
+  $('#shots').html('<img src="assets/fireball.png"> X '+inventory['shots']);
+  $('#mana .c100').removeClass();
+  $('#mana #manaDisc').addClass('c100 small dark p'+manaPercent);
+  $('#mana .c100 span').html(manaPercent+'%');
+  $('.elements #ballons').html('Ballons restantes: x '+enemiesNum);
+  if(inventory['shots'] > 0){
+    $('#shots').css('background', 'rgba(255, 255, 255, 0.25)');
+    $('#shots').css('border', '1px solid rgba(255, 255, 255, 1)');
+  }
+  if(manaPercent == 100 || (manaPercent <= 15 && loadAttacks == true)){
+    loadAttacks = true;
+    $('#mana .plus1').fadeIn(150);
+  }
+  else{
+    $('#mana .plus1').fadeOut(300);
+  }
+
+}
+
+// Banner
+function randomBanner(){
+  for(var i = 0; i < 25; i++){
+    var x = Math.floor(Math.random() * ($(document).width() - 0) + 0);
+    var y = Math.floor(Math.random() * ($(document).height() - 0) + 0);
+    var rotation = Math.floor(Math.random() * (90 - 0) + 0);
+    $('.banner').append('<img class="spirit" src="assets/darkSpirit.png" style="top: '+x+'px; left: '+y+'px; transform: rotate('+rotation+'deg)">');
+  }
+}
+
+// Screens
+function controls(){
+  $('.startGame #presentation').fadeOut(50);
+  $('.startGame #controls').fadeIn(500);
+}
+
+function presentation(){
+  $('.startGame #controls').fadeOut(50);
+  $('.startGame #presentation').fadeIn(500);
 }
